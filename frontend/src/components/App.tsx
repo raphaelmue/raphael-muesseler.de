d080a29c2ac96402acfc68e6de1e8929abfabc92
Raphael Müßeler <raphael.muesseler@sap.com>
2021-07-26 18:55:38 +0200

    master: fixed starting issues

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home  from './home/Home';
 
 function App() {
     return (
         <div>
-            test
+            <Home/>
         </div>
     );
 }
--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React                                                   from 'react';
+import {Container, Nav, Navbar, NavbarBrand, NavItem, NavLink} from 'reactstrap';
+import 'argon-design-system-react/src/assets/vendor/nucleo/css/nucleo.css';
+import 'argon-design-system-react/src/assets/vendor/font-awesome/css/font-awesome.min.css';
+import 'argon-design-system-react/src/assets/css/argon-design-system-react.css';
 
 interface HomeComponentProps {
 }
@@ -9,7 +13,31 @@ interface HomeComponentState {
 class Home extends React.Component<HomeComponentProps, HomeComponentState> {
 
     render() {
-        return <div/>;
+        return (
+            <Navbar
+                className="navbar-horizontal navbar-dark bg-default"
+                expand="lg">
+                <Container>
+                    <NavbarBrand onClick={e => e.preventDefault()}>
+                        Raphael Müßeler
+                    </NavbarBrand>
+                    <Nav>
+                        <NavItem>
+                            <NavLink
+                                className="nav-link-icon"
+                                href="#pablo"
+                                onClick={e => e.preventDefault()}>
+                                <span className="nav-link-inner--text d-lg-none">
+                                    Discover
+                                </span>
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Container>
+            </Navbar>
+        );
     }
 
 }
+
+export default Home;
